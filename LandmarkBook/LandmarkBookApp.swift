e6038bd20097104f48d9ccedbf8493e8c1ad5a75
Atakan Cengiz KURT <atakan.kurt@loodos.com>
2021-10-05 21:52:45 +0300

    Initial Commit

---
+++ LandmarkBook/LandmarkBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkBookApp.swift
+//  LandmarkBook
+//
+//  Created by Atakan Cengiz KURT on 5.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
